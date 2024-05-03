ebfbb1aeb266f1c99fa61320d939fc03aede0d64
Phuc Dinn <phucdinn1803@gmail.com>
2024-05-03 11:03:09 +0700

    update

--- yuuki/lib/main.dart
+++ yuuki/lib/main.dart
@@ -2,11 +2,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:yuuki/models/my_user.dart';
 import 'package:yuuki/my_sign_up_page.dart';
+import 'package:yuuki/screens/home_screen.dart';
 import 'package:yuuki/services/user_service.dart';
 import 'package:yuuki/listeners/on_user_create_listener.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:yuuki/utils/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
@@ -26,13 +28,11 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Search User'),
-        ),
         body: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: MySignUpPage(),
+          child: HomeScreen(),
         ),
       ),
     );
